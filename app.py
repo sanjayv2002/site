1ee37b7d4f0b5c024e9e432213e6538392adc638
sanjayv2002 <sanjay.murthy.29@gmail.com>
2023-03-04 19:10:30 +0530

    skeleton complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, send_file
 from flask_cors import cross_origin
 
 app = Flask(__name__)
@@ -28,6 +28,7 @@ def intro():
     return render_template("intro.html")
 
 
+
 @app.route("/round1")
 @cross_origin()
 def display_round1():
@@ -38,12 +39,18 @@ def display_round1():
 def submit_round1():
     submission = request.form['submission']
 
-    if submission == 'DUMmy':
+    if submission == '992.6.8.1':
         flash("Success","info")
-        return redirect(url_for("display_round2"))
+        return redirect(url_for("round2pre"))
     else:
         flash("Try Again!, you got this!","error")
         return redirect(url_for("display_round1"))
+    
+@app.route("/round2pre")
+@cross_origin()
+def round2pre():
+    return render_template("page5.html")
+
 
 @app.route("/round2")
 @cross_origin()
@@ -55,29 +62,48 @@ def display_round2():
 def submit_round2():
     submission = request.form['submission']
 
-    if submission == 'DUMmy':
+    if submission == 'pbueufmvducghopamv':
         flash("Success","info")
-        return redirect(url_for("display_round3"))
+        return redirect(url_for("round3pre"))
     else:
         flash("Try Again!, you got this!","error")
         return redirect(url_for("display_round2"))
     
+@app.route("/round3pre")
+@cross_origin()
+def round3pre():
+    return render_template("page7.html")
+
+
 @app.route("/round3")
 @cross_origin()
 def display_round3():
     return render_template("round3.html")
+
+@app.route("/download")
+@cross_origin()
+def download():
+    path_file = "static\Archive.rar"
+    filename = "Archive.rar"
+    return send_file(path_file, as_attachment=True)
+
 
 @app.route("/submitround3", methods=["POST"])
 @cross_origin()
 def submit_round3():
     submission = request.form['submission']
 
-    if submission == 'DUMmy':
+    if submission == ' (0u9_D3_gRa{3_[YRU5':
         flash("Success","info")
-        return redirect(url_for("display_round4"))
+        return redirect(url_for("round4pre"))
     else:
         flash("Try Again!, you got this!","error") 
         return redirect(url_for("display_round3"))
+    
+@app.route("/round4pre")
+@cross_origin()
+def round4pre():
+    return render_template("page9.html")
     
 @app.route("/round4")
 @cross_origin()
@@ -89,7 +115,7 @@ def display_round4():
 def submit_round4():
     submission = request.form['submission']
 
-    if submission == 'DUMmy':
+    if submission == r'flag{411_ur_37h_15_m1n3}':
         flash("Success","info")
         return redirect(url_for("success"))
     else:
@@ -101,6 +127,11 @@ def submit_round4():
 @cross_origin()
 def success():
     return render_template("success.html")
+
+@app.route("/bubyee")
+@cross_origin()
+def bubyee():
+    return render_template("bye.html")
 
 
 if __name__ == "__main__":
